2420458509930a13b01f8b2e0702dfc013645e54
Humphrey Kamau <58790731+Grayhkamau@users.noreply.github.com>
2021-09-11 12:57:55 +0300

    connected login and sign up to home page

---
+++ server/auth/facebook.js
@@ -0,0 +1,37 @@
+var passport = require('passport');
+var config = require('../_config');
+var init = require('./init');
+var User = require('../models/user');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+    clientID: config.facebook.consumerKey,
+    consumerSecret: config.facebook.consumerSecret,
+    callbackURL:"http://127.0.0.1:3000/auth/facebook/callback"
+  },
+    function(token, tokenSecret, profile, done) {
+
+    var searchQuery = {
+      name: profile.displayName
+    };
+
+    var updates = {
+      name: profile.displayName,
+      someID: profile.id
+    };
+
+    var options = {
+      upsert: true
+    };
+
+      User.findOneAndUpdate(searchQuery, updates, options, function(err, user) {
+      if(err) {
+        return done(err);
+      } else {
+        return done(null, user);
+      }
+    });
+  }
+
+));
+module.exports = passport;
